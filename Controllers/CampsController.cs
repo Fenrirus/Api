55142974c0f9689bea1ba21585205efd2f417e71
Marian Marian <fenrir55@gmail.com>
2020-04-20 20:20:37 +0200

    get

---
+++ Controllers/CampsController.cs
@@ -0,0 +1,31 @@
+﻿using CoreCodeCamp.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {
+        private readonly ICampRepository campRepository;
+
+        public CampsController(ICampRepository campRepository)
+        {
+            this.campRepository = campRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCamps()
+        {
+            try
+            {
+                var results = await campRepository.GetAllCampsAsync();
+                return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(500, "Database Failure");
+            }
+        }
+    }
+}
